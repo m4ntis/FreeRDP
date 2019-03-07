fc7753af0b9ed670f74b949235e4105f49651fbc
kubistika <kmizrachi18@gmail.com>
2019-03-07 17:24:45 +0200

    server/proxy: Add graphics callbacks

--- server/proxy/pf_server.c
+++ server/proxy/pf_server.c
@@ -51,12 +51,16 @@ void pf_server_handle_client_disconnection(freerdp_peer* client)
 	proxyContext* pContext = (proxyContext*)client->context;
 	clientToProxyContext* cContext = (clientToProxyContext*)client->context;
 	rdpContext* sContext = pContext->peerContext;
+
 	WLog_INFO(TAG, "Client %s disconnected; closing connection with server %s", client->hostname,
-	          sContext->settings->ServerHostname);
+	sContext->settings->ServerHostname);
+
 	WLog_INFO(TAG, "connectionClosed event is not set; closing connection to remote server");
+
 	/* Mark connection closed for sContext */
 	SetEvent(pContext->connectionClosed);
 	freerdp_abort_connect(sContext->instance);
+
 	/* Close connection to remote host */
 	WLog_DBG(TAG, "Waiting for proxy's client thread to finish");
 	WaitForSingleObject(cContext->thread, INFINITE);
@@ -120,11 +124,13 @@ BOOL pf_peer_post_connect(freerdp_peer* client)
 	rdpContext* sContext = proxy_to_server_context_create(client->context,
 	                       host, port, username, password);
 	/* Inject proxy's client context to proxy's context */
+
 	HANDLE connectionClosedEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
 	pContext->peerContext = sContext;
 	pContext->connectionClosed = connectionClosedEvent;
 	((proxyContext*)sContext)->peerContext = (rdpContext*)pContext;
 	((proxyContext*)sContext)->connectionClosed = connectionClosedEvent;
+
 	clientToProxyContext* cContext = (clientToProxyContext*)client->context;
 
 	if (!(cContext->thread = CreateThread(NULL, 0, proxy_client_start, sContext, 0, NULL)))
