9176cedddd4280e202af02288ee25c86dcec601d
Armin Novak <armin.novak@gmail.com>
2019-05-02 07:03:16 +0200

    Fixed const correctness of gfx function pointer
    Signed-off-by: Mati Shabtay <matishabtay@gmail.com>

--- include/freerdp/client/rdpgfx.h
+++ include/freerdp/client/rdpgfx.h
@@ -87,9 +87,9 @@ typedef UINT(*pcRdpgfxOnOpen)(RdpgfxClientContext* context, BOOL* do_caps_advert
                               BOOL* do_frame_acks);
 typedef UINT(*pcRdpgfxOnClose)(RdpgfxClientContext* context);
 typedef UINT(*pcRdpgfxCapsAdvertise)(RdpgfxClientContext* context,
-                                     RDPGFX_CAPS_ADVERTISE_PDU* capsAdvertise);
+                                     const RDPGFX_CAPS_ADVERTISE_PDU* capsAdvertise);
 typedef UINT(*pcRdpgfxCapsConfirm)(RdpgfxClientContext* context,
-                                   RDPGFX_CAPS_CONFIRM_PDU* capsConfirm);
+                                   const RDPGFX_CAPS_CONFIRM_PDU* capsConfirm);
 typedef UINT(*pcRdpgfxFrameAcknowledge)(RdpgfxClientContext* context,
                                         const RDPGFX_FRAME_ACKNOWLEDGE_PDU* frameAcknowledge);
 
